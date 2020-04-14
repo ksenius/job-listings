51e507a2f74d892f913c85c4b759b7ad9f7436ef
ksenia <30226190+ksenius@users.noreply.github.com>
2020-04-14 20:58:56 +0700

    Create layout component, update base.pug

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import '../scss/global.scss';
 // import '../components/**/*.js';
+import '../components/layout/layout';
 import '../components/header/header';
 import '../components/footer/footer';
