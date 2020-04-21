1a2b3c7e8e6893ae7f099ece1456c0865072a6f0
ksenia <30226190+ksenius@users.noreply.github.com>
2020-04-21 20:19:49 +0700

    Install anime.js, update job-list

--- src/components/job-list/job-list.js
+++ src/components/job-list/job-list.js
@@ -3,33 +3,46 @@ import '../job-card/job-card';
 import '../filter/filter';
 import './job-list.scss';
 import Filter from '../filter/filter';
+import anime from 'animejs/lib/anime.es.js';
 
 class JobList {
   constructor(element) {
     this.element = element;
 
-    this._getTagElements();
+    this._getElements();
     this._createFilter();
   }
 
   init() {
-    this._bindEventListeners();
+    this._animateJobCards();
+    this._attachTagClickHandler();
   }
 
-  _getTagElements() {
+  _getElements() {
+    this.jobCards = this.element.querySelectorAll('.js-job-list__card');
     this.tags = this.element.querySelectorAll('.js-job-card__tag');
   }
 
   _createFilter() {
-    const jobCards = this.element.querySelectorAll('.js-job-list__card');
     const container = this.element.querySelector('.js-job-list__container');
 
-    this.filter = new Filter(jobCards, container);
+    this.filter = new Filter(this.jobCards, container);
 
     this.filter.element.classList.add('job-list__filter');
   }
 
-  _bindEventListeners() {
+  _animateJobCards() {
+    anime({
+      targets: this.jobCards,
+      translateY: [50, 0],
+      opacity: [0, 1],
+      duration: 200,
+      delay: anime.stagger(100, { start: 200 }),
+      easing: 'easeOutQuad',
+    });
+  }
+
+  _attachTagClickHandler() {
     this.tags.forEach((tag) => {
       tag.addEventListener('click', (event) => {
         this._handleTagClick(event);
